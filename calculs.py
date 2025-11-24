ab5e4a52b750072b6d75a65146383eb1facba0d7
MÉLANIE SOUCHU <melanie.souchu@nomachine.univ-eiffel.fr>
2025-11-24 11:26:24 +0100

    rough base of what will be in there

---
+++ calculs.py
@@ -0,0 +1,17 @@
+def wgs_to_mercator():
+    pass
+
+def mercator_to_wgs():
+    pass
+
+def import_shp(file_name):
+    # shapefile
+    pass
+
+def import_csv(file_name):
+    # csv
+    pass
+
+def import_netcdf(file_name):
+    # module netCDF4
+    pass
