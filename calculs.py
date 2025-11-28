05e0b4b1ca73940b5f341453f5f451ffbe0fc2b2
MÉLANIE SOUCHU <melanie.souchu@nomachine.univ-eiffel.fr>
2025-11-28 12:13:24 +0100

    mercator and points

--- calculs.py
+++ calculs.py
@@ -1,13 +1,40 @@
 from requirements import shapefile as shapefile
 import typing
+import pyexcel_xlsx as pyxl
+import json
+import math
+
 #import shapefile
 
-def wgs_to_mercator():
-    pass
+def wgs_to_mercator(pos) -> typing.Tuple[float, float]:
+    R : float = 6378137.000
+    lat, lon = pos[0], pos[1]
+
+    x = R * math.radians(lon)
+    scale = x / lon
+    pi = math.pi
+    y = 180 / pi * math.log(math.tan(pi/4.0 + lat * (pi/180.0)/2.0)) * scale
+
+    return (x, y)
 
 
 def mercator_to_wgs():
     pass
+
+
+def convert_wgs_to_mercator(departments):
+    departments_mercator : typing.Dict[str, typing.List[str, typing.List[typing.Tuple[float, float]]]] = { 
+
+    }
+
+    for department in departments:
+        new_points : typing.List[typing.Tuple[float, float]] = [ ]
+        for point in departments[department][1]:
+            new_point = wgs_to_mercator(point)
+            new_points.append(new_point)
+        departments_mercator[department] = [ departments[department][0], new_points ]
+
+    return departments_mercator
 
 
 def import_csv(file_name):
@@ -24,25 +51,67 @@ def import_netcdf(file_name):
 
 
 def import_shp(file_name) -> shapefile:
+    """
+    Parameter: 
+        file_name : str = name/path of the shapefile (.shp)
+    Returns:
+        sf : shapefile = the content of the shapefile file
+    Imports a shp file and returns its content
+    
+    """
     # shapefile
     sf : shapefile = shapefile.Reader(file_name)
 
     return sf
 
 def get_points(sf) -> typing.Dict[str, int]:
-    departements : typing.Dict[str, int] = {
+    """
+    Parameter:
+        sf : shapefile = content of a shapefile file
+    Returns:
+        departments : typing.Dict[str, typing.List[str, typing.List[typing.Tuple[int, int]]]] = dictionnary where:
+            keys : str = are the departments' ID
+            values : list with list[0] being the name (str) and list[1] being the list of tuples = are the points making the outline of each department
+    Take the IDs and list of points from the content of the shapefile and returns a dictionnary with the departments' ID and their outlines
+    """
+
+    departments : typing.Dict[str, typing.List[str, typing.List[typing.Tuple[int, int]]]] = {
 
     }
     sf_shapes = sf.shapes()
 
     for i in range(len(sf_shapes)):
         curr_record = sf.record(i)
-        departements[curr_record[0]] = [curr_record[1], sf.shape(i).points]
-    return departements
+        departments[curr_record[0]] = [curr_record[1], sf.shape(i).points]
+    return departments
     
 
-sf = shapefile.Reader("departements-20180101-shp.zip/departements-20180101.shp")
-print(sf.records())
-print(sf.shape(0))
-print(sf.record(0)[0])
-print(get_points(sf)["30"])
+# sf = shapefile.Reader("departements-20180101-shp.zip/departements-20180101.shp")
+# print(sf.records())
+# print(sf.shape(0))
+# print(sf.record(0)[0])
+# print(get_points(sf)["30"])
+
+sf = import_shp("departements-20180101-shp.zip/departements-20180101.shp")
+points = get_points(sf)
+#print(points["30"][1])
+#print(convert_wgs_to_mercator(points))
+
+mercator_points = convert_wgs_to_mercator(points)
+
+
+# -------------------------------XLXS----------------------------------------
+
+
+# def import_xlsx(file_name):
+#     data = pyxl.get_data(file_name)
+#     print(json.dump(data, dict)["III_1_insee_population_fr_depar"][0])
+
+# import_xlsx("POPULATION_MUNICIPALE_DEPARTEMENT_FRANCE.xlsx")
+
+
+# -------------------------------CSV----------------------------------------
+
+
+
+l
